c468d8398c213a33bf5e7ec4a2abe67305e63067
tea <tzivkovic500@gmail.com>
2018-02-16 13:28:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/singlescreenappproject1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.singlescreenappproject1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // Display discount information on the screen
+    public void sendButton(View view) {
+        String discountMessage = "Congratulations you won 10% discount on the product you choose!!!" + "\nVisit our shop and show this message and get the discount." + "\nHappy shopping!!!" + "\nNew Tehnologies Team";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Congratulations you won 10% discount!!!");
+        intent.putExtra(Intent.EXTRA_TEXT, discountMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+        startActivity(intent);
+    }
+
+        // Display web
+        TextView t2 = (TextView) findViewById(R.id.text2);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+    }
+}
+
+
+
+
+
+
+
